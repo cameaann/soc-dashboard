48d45d4100d442b0f96c0c132707876584f4b45f
Anna <cameaann@gmail.com>
2024-04-24 21:09:12 +0300

    add and update time params in the logs path

---
+++ src/components/LogRecord.jsx
@@ -0,0 +1,37 @@
+import dayjs from "dayjs";
+
+const LogRecord = ({ log, serviceType, logsName }) => {
+  let date = dayjs(log.timestamp).format("DD-MM-YYYY");
+  let time = dayjs(log.timestamp).format("HH:MM");
+
+  if (serviceType === "server" && logsName === "login-attempts") {
+    return (
+      <li className="log">
+        Käyttäjä <span className="user">{log.username}</span> kirjautui
+        palvelimelle <span className="service">{log.server_name}</span>
+        {log.status === "success" ? " onnistuneesti" : " epäonnistuneesti"}{" "}
+        <span className="date"> {date} </span>klo{" "}
+        <span className="time">{time}</span>
+      </li>
+    );
+  }
+  if (serviceType === "server" && logsName === "updates") {
+    return (
+      <li className="log">
+        Palvelimella {log.server_name} asennettiin päivitys {log.update_name}
+        {log.status === "success" ? " onnistuneesti" : " epäonnistuneesti"}{" "}
+        <span className="date"> {date} </span>klo {time}
+      </li>
+    );
+  }
+  if (serviceType === "firewall" && logsName === "firewall-distribution") {
+    return (
+      <li className="log">
+        Palamuuri {log.action} yhteydenoton IP-osoitteesta {log.destination_ip}{" "}
+        porttiin {log.destination_port} ({log.protocol}){" "}
+        <span className="date"> {date} </span>klo {time}
+      </li>
+    );
+  }
+};
+export default LogRecord;
